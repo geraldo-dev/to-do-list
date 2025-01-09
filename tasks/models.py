cd8a9d6c655678449e88fd7e9477e5e7d03d9893
geraldo ferreira <geraldosousadev@gmail.com>
2025-01-08 21:21:31 -0300

    creating task tables in the database

--- tasks/models.py
+++ tasks/models.py
@@ -1,3 +1,10 @@
 from django.db import models
 
 # Create your models here.
+class Task(models.Model):
+    user_id = models.AutoField(primary_key=True)
+    name_task = models.TextField(max_length=100, default='')
+    created_date = models.DateTimeField(default='')
+
+    def __str__(self):
+        return f'task: {self.name_task}'
