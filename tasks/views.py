63776d3cc410a7b3876afb9dff0205e27006e4e8
geraldo ferreira <geraldosousadev@gmail.com>
2025-01-10 09:01:14 -0300

    validating fields with forms

--- tasks/views.py
+++ tasks/views.py
@@ -1,58 +1,55 @@
 
 from django.shortcuts import redirect, render, get_object_or_404
 from django.utils import timezone
-from django.contrib import messages
+# from django.contrib import messages
+from .forms import TaskCreateinForm
 from .models import Task
 
 
 def index(request):
-    all_tasks = Task.objects.all()
-    data = { "all_tasks": all_tasks}
+    form = TaskCreateinForm()
 
     if request.method == 'GET':
 
-        return render(request, 'tasks/index.html', data)
+        all_tasks = Task.objects.all().order_by('-created_date')
+        return render(request, 'tasks/index.html', { "all_tasks": all_tasks, 'form':form})
     
     elif request.method == 'POST':
 
-        name = request.POST.get('name_task')
+        form = TaskCreateinForm(request.POST)
 
-        task = Task()
-
-        if not name.strip():
-            return redirect('index')
-        else:
-            task.name_task = name
+        if form.is_valid():
+            task = form.save(commit=False)
             task.created_date = timezone.now()
             task.save()
-        return redirect('index')
+            return redirect('detail', id=task.task_id)
+        else:
+            return render(request, 'tasks/index.html', { "all_tasks": all_tasks, 'form':form})
 
 def detail(request, id):
     try:
         task = get_object_or_404(Task,pk=id)
     except:
-        return redirect("index")
+        return redirect("not_found")
     return render(request, 'tasks/detail.html', {'task':task})
 
 def update(request, id):
 
-    if request.method == 'GET':
+    task = get_object_or_404(Task,pk=id)
 
-        task = get_object_or_404(Task,pk=id)
-        return render(request, 'tasks/update.html', {'task':task})
+    if request.method == 'GET':
+        form = TaskCreateinForm(instance=task)
+        return render(request, 'tasks/update.html', {'form': form, 'task':task})
     
     elif request.method == 'POST':
         
-        name = request.POST.get('name_task')
-
-        task = Task.objects.get(pk=id)
-        
-        if not name.strip():
-            return render(request, 'tasks/update.html', {'task':task})
-        else:
-            task.name_task = name
-            task.save()
-        return render(request, 'tasks/detail.html', {'task':task})
+        form = TaskCreateinForm(request.POST, instance=task)
+        if form.is_valid():
+            form.save()
+            return redirect('detail',  id=task.task_id)
+    else:
+        form = TaskCreateinForm(instance=task)
+    return render(request, 'tasks/update.html', {'form': form})
 
 def delete(request, id):
 
