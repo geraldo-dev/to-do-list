db010b5ca7a0d47ec9d8a15d6eccd6cbcf800985
geraldo ferreira <geraldosousadev@gmail.com>
2025-01-10 08:08:37 -0300

    forms created and updated task

---
+++ tasks/forms.py
@@ -0,0 +1,22 @@
+from django import forms
+from .models import Task
+
+
+class TaskCreateinForm(forms.ModelForm):
+    class Meta:
+        model = Task
+        fields = ['name_task','created_date']
+    
+        widgets = {
+            'name_tasks' : forms.TextInput(attrs={'class':'form-control'}),
+        }
+
+
+class TaskUpdateForm(forms.ModelForm):
+    class Meta:
+        model = Task
+        fields = ['name_task']
+    
+        widgets = {
+            'name_tasks' : forms.TextInput(attrs={'class':'form-control'}),
+        }
